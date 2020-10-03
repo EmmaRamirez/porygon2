d60c63d57d69fe5348b0d709927fdd6f9829b446
emmaramirez <ramirez.emma.g@gmail.com>
2020-10-02 18:33:24 -0600

    feat(button): add further button stylings

--- core/components/Button/Button.stories.tsx
+++ core/components/Button/Button.stories.tsx
@@ -2,25 +2,35 @@ import React from 'react';
 import { Story, Meta } from '@storybook/react';
 
 import { Button, ButtonProps } from './Button';
+import { Intent } from 'types';
 
 export default {
   title: 'Components/Button',
   component: Button,
-  argTypes: {
-    backgroundColor: { control: 'color' },
-  },
 } as Meta;
 
 const Template: Story<ButtonProps> = (args) => <Button {...args} />;
 
 export const Primary = Template.bind({});
 Primary.args = {
-    children: <strong>Button</strong>,
-    defaultValue: 'Button',
+    children: 'Button',
 };
 
 export const Ghost = Template.bind({});
 Ghost.args = {
-  children: <strong>Button</strong>,
+  children: 'Button',
   ghost: true,
 };
+
+
+export const Disabled = Template.bind({});
+Disabled.args = {
+  children: 'Button',
+  disabled: true,
+};
+
+export const IntentDanger = Template.bind({});
+IntentDanger.args = {
+  children: 'Button',
+  intent: Intent.Danger,
+}
--- core/components/Button/Button.tsx
+++ core/components/Button/Button.tsx
@@ -5,25 +5,42 @@ import { Theme } from 'theme';
 import {Intent} from 'types';
 import { getContrastColor, Styles, useStyles } from 'utils';
 import { createBackground } from 'utils/createBackground';
-import { interactable } from 'types/Interactable';
 
 const styles: Styles = {
     base: {
         background: Theme.PoryBlue,
         color: getContrastColor(Theme.PoryBlue),
         border: `1px solid ${color(Theme.PoryBlue).darken(0.2).toString()}`,
-        padding: `0.25rem .5rem`,
-        borderRadius: '0.33rem',
+        padding: `0.25rem .75rem`,
+        borderRadius: '0.2rem',
         fontSize: '0.85rem',
         cursor: 'pointer',
+        boxShadow: `0 0 2px rgba(0, 0, 0, 0.2)`,
+    },
+    focus: {
+        outline: `2px solid ${Theme.PoryBlue}`,
     },
     ghost: {
         ...createBackground(Theme.Background),
         border: `1px solid ${color(Theme.PoryBlue)}`,
     },
     disabled: {
+        ...createBackground(color(Theme.PoryBlue).desaturate(0.5).toString()),
 
-    }
+    },
+    [Intent.Primary]: {
+        ...createBackground(Theme.PoryBlue),
+    },
+    [Intent.Warn]: {
+
+    },
+    [Intent.Danger]: {
+        ...createBackground(Theme.PoryRed),
+    },
+    [Intent.Success]: {
+
+    },
+    
 };
 
 export interface ButtonProps extends React.DetailedHTMLProps<ButtonHTMLAttributes<HTMLButtonElement>, HTMLButtonElement> {
@@ -31,6 +48,9 @@ export interface ButtonProps extends React.DetailedHTMLProps<ButtonHTMLAttribute
     ghost?: boolean;
 }
 
+/**
+ *  Buttons trigger actions when clicked.
+ */
 export function Button({
     ghost,
     disabled,
@@ -40,8 +60,9 @@ export function Button({
     const cx = useStyles(styles);
     const className = classes(
         cx.base,
+        cx[intent ?? 'primary'],
+        disabled && cx.disabled,
         ghost && cx.ghost,
-        disabled && interactable.disabled,
     );
 
     return <button className={className} data-testid='button' {...props} />
